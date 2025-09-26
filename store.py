9ca9d1209d8d77489eae72437d243f4428a213ca
lahrour88 <abdelaadime@zohomail.com>
2025-09-26 11:52:39 +0200

    add pillow lib

--- store.py
+++ store.py
@@ -26,18 +26,3 @@ class users:
         self.is_admin=is_admin
         self.profile_filename =profile_filename
 
-data={'id': 334,
-       'body': 'e\r\ne\r\ne\r\ne\r\ne\r\ne\r\ne\r\ne\r\ne\r\ne\r\ne',
-         'page': 'sport',
-           'title': 'eeeeeeeeeeeeeee',
-             'date': '2025-09-25',
-               'img1': 'https://biytrshphtxlywabygcc.supabase.co/storage/v1/object/public/images/posts/1b8e3d7b38cd423d912473a981cbc9c0_1758827483.png',
-                'name': 'abdelaadime lahrour',
-                'topic': '',
-                'img2': 'https://biytrshphtxlywabygcc.supabase.co/storage/v1/object/public/images/posts/4c1973dcbbdd4182bb6e9119b627f730_1758827484.png',
-                'img3': 'https://biytrshphtxlywabygcc.supabase.co/storage/v1/object/public/images/posts/0ff3ab0a0c004bbe98e6bcc336a18f05_1758827484.png',
-                'img4': 'https://biytrshphtxlywabygcc.supabase.co/storage/v1/object/public/images/posts/8e583934470a4399b3a677fab32f9998_1758827484.png',
-                'img1_name': '1b8e3d7b38cd423d912473a981cbc9c0_1758827483.png',
-                'img2_name': '4c1973dcbbdd4182bb6e9119b627f730_1758827484.png',
-                'img3_name': '0ff3ab0a0c004bbe98e6bcc336a18f05_1758827484.png',
-                'img4_name': '8e583934470a4399b3a677fab32f9998_1758827484.png'}
